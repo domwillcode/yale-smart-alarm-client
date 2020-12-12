bfcd36475c5441bc6782f1595245cc597a12ef2c
Dominic Willson <dominic.willson@gmail.com>
2020-12-12 11:36:26 +0000

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='yalesmartalarmclient',
     py_modules=['yalesmartalarmclient'],
-    version='0.1.7',
+    version='0.3.0',
     description='Interact with Yale Smart Alarm systems',
     long_description=long_description,
     long_description_content_type="text/markdown",
