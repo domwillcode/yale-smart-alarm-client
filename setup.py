d398c3d906a46278c5c809922a3fd5d5467c8778
Mr-Grover <52253935+Mr-Grover@users.noreply.github.com>
2021-04-15 22:59:37 +0100

    added backoff (#27)
    Co-authored-by: sim_rig <jottofar@gmail.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     download_url='https://github.com/domwillcode/yale-smart-alarm-client',
     keywords=['alarm', 'Yale', 'Smart Alarm'],
     package_data={'': ['data/*.json']},
-    install_requires=['requests>=2.0.0'],
+    install_requires=['requests>=2.0.0','backoff==1.10.0'],
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
