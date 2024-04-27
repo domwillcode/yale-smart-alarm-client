17bd6c03dc7d0e5be69eeff5e2dce80390c53e63
Jakob Helgesson <jakob.helgesson@gmail.com>
2024-04-27 21:36:31 +0200

    Introduce lock config methods

--- yalesmartalarmclient/lock.py
+++ yalesmartalarmclient/lock.py
@@ -13,7 +13,6 @@ from .exceptions import AuthenticationError
 if TYPE_CHECKING:
     from .auth import YaleAuth
 
-
 class YaleLockState(Enum):
     """Lock state enum."""
 
@@ -22,6 +21,62 @@ class YaleLockState(Enum):
     DOOR_OPEN = 3
     UNKNOWN = 4
 
+class YaleLockVolume(Enum):
+    """Lock volume enum."""
+
+    HIGH = "03"
+    LOW = "02"
+    OFF = "01"
+
+class YaleLockConfig:
+    """Represents the configuration of a lock."""
+
+    def __init__(self, conf_data: str) -> None:
+        """Initialize the lock configuration."""
+        self.volume = conf_data[0:2]
+        self.autolock = conf_data[2:4]
+        self.language = conf_data[8:10]
+        self.arm_hold_time = conf_data[30:32]
+
+    def __str__(self) -> str:
+        """Return a string representation of the lock configuration."""
+        return f"volume: {self.volume}, autolock: {self.autolock}, language: {self.language}, armHoldTime: {self.arm_hold_time}"
+
+    def __eq__(self, other: object) -> bool:
+        """Compare two lock configurations."""
+        if isinstance(other, self.__class__):
+            return (
+                self.volume == other.volume
+                and self.autolock == other.autolock
+                and self.language == other.language
+                and self.arm_hold_time == other.arm_hold_time
+            )
+        return False
+
+    def __ne__(self, other: object) -> bool:
+        """Compare two lock configurations."""
+        return not self.__eq__(other)
+
+    def __hash__(self) -> int:
+        """Return the hash of the lock configuration."""
+        return hash((self.volume, self.autolock, self.language, self.arm_hold_time))
+
+    def to_dict(self) -> dict[str, str]:
+        """Return the lock configuration as a dictionary."""
+        return {
+            "volume": self.volume,
+            "autolock": self.autolock,
+            "language": self.language,
+            "armHoldTime": self.arm_hold_time,
+        }
+
+    def to_string(self) -> str:
+        # """Return the lock configuration as a string."""
+        conf = "0" * 32
+        conf = conf[:0] + self.volume + conf[2:]
+        conf = conf[:2] + self.autolock + conf[4:]
+        conf = conf[:8] + self.language + conf[10:]
+        conf = conf[:30] + self.arm_hold_time + conf[32:]
 
 class YaleLock:
     """This is an abstraction of a remove Yale lock.
@@ -39,6 +94,7 @@ class YaleLock:
         self._lock_api = lock_api
         self._device: dict[str, Any] = device
         self.name: str = device["name"]
+        self._config: YaleLockConfig = YaleLockConfig(device["minigw_configuration_data"])
         self._state: YaleLockState = YaleLockState.UNKNOWN
         self.update(device)
 
@@ -59,6 +115,7 @@ class YaleLock:
         self._device = device
         self.name = device["name"]
         self._state = self._calc_state()
+        self._config = YaleLockConfig(device["minigw_configuration_data"])
 
     def state(self) -> YaleLockState:
         """Return the lock state of the lock."""
@@ -131,7 +188,38 @@ class YaleLock:
             raise error
         except RequestException as error:
             raise error
+    
+    def set_volume(self, volume: YaleLockVolume) -> bool:
+        """Set the volume of the lock.
 
+        Args:
+            volume: the new volume. high, low or off.
+
+        Returns:
+            True if the operation was a success.
+        """
+        try:
+            return self._lock_api.set_volume(lock=self, volume=volume)
+        except AuthenticationError as error:
+            raise error
+        except RequestException as error:
+            raise error
+    
+    def set_auto_lock(self, auto_lock: bool) -> bool:
+        """Set the auto lock state of the lock.
+
+        Args:
+            auto_lock: the new state of the auto lock.
+
+        Returns:
+            True if the operation was a success.
+        """
+        try:
+            return self._lock_api.set_auto_lock(lock=self, auto_lock=auto_lock)
+        except AuthenticationError as error:
+            raise error
+        except RequestException as error:
+            raise error
 
 class YaleDoorManAPI:
     """Represents the yale doorman api.
@@ -168,6 +256,8 @@ class YaleDoorManAPI:
     # lock door
     _ENDPOINT_DEVICES_CONTROL = "/api/panel/device_control/"
     _ENDPOINT_DEVICES_UNLOCK = "/api/minigw/unlock/"
+    _ENDPOINT_DEVICES_CONFIG = "/api/minigw/lock/config/"
+    _ENDPOINT_DEVICES_UPDATE = "/api/panel/device/"
 
     def __init__(self, auth: "YaleAuth") -> None:
         """Initialize the module."""
@@ -296,4 +386,79 @@ class YaleDoorManAPI:
         success: bool = operation_status["code"] == self.CODE_SUCCESS
         if success:
             lock.set_state(YaleLockState.UNLOCKED)
+        return success
+    
+    def _put_lock_request(self, lock: YaleLock) -> bool:
+        """Api endpoints that seems to be called after device update. Not sure what it does or if it is needed.
+        In the yale app it is called after setting auto lock, volume, language, name, when device is deleted, when updateDevice is called (Not sure what that is) and when device is added.
+        """
+        try:
+            operation_status = self.auth.put_authenticated(self._ENDPOINT_DEVICES_UPDATE)
+        except AuthenticationError as error:
+            raise error
+        except RequestException as error:
+            raise error
+        success: bool = operation_status["code"] == self.CODE_SUCCESS
+        return success
+
+    def set_volume(self, lock: YaleLock, volume: YaleLockVolume) -> bool:
+        """Set the volume of the lock.
+
+        Args:
+            lock: the lock to set the volume for.
+            volume: the new volume. high, low or off.
+
+        Returns:
+            True if the operation was a success.
+        """
+        params = {
+            "area": lock.area(),
+            "zone": lock.zone(),
+            "val": volume.value,
+            "idx": "01" # Hardcoded value in the app
+        }
+        try:
+            operation_status = self.auth.post_authenticated(
+                self._ENDPOINT_DEVICES_CONFIG, params=params
+            )
+        except AuthenticationError as error:
+            raise error
+        except RequestException as error:
+            raise error
+        success: bool = operation_status["code"] == self.CODE_SUCCESS
+        if success:
+            lock._config.volume = volume
+            # For some reason the app calls _put_lock_request after setting volume
+            return self._put_lock_request(lock)
+        return success
+    
+    def set_auto_lock(self, lock: YaleLock, auto_lock: bool) -> bool:
+        """Set the auto lock state of the lock.
+
+        Args:
+            lock: the lock to set the auto lock state for.
+            auto_lock: the new state of the auto lock.
+
+        Returns:
+            True if the operation was a success.
+        """
+        params = {
+            "area": lock.area(),
+            "zone": lock.zone(),
+            "val": "FF" if auto_lock else "00", # Hardcoded value in the app
+            "idx": "02" # Hardcoded value in the app
+        }
+        try:
+            operation_status = self.auth.post_authenticated(
+                self._ENDPOINT_DEVICES_CONFIG, params=params
+            )
+        except AuthenticationError as error:
+            raise error
+        except RequestException as error:
+            raise error
+        success: bool = operation_status["code"] == self.CODE_SUCCESS
+        if success:
+            lock._config.autolock = "FF" if auto_lock else "00"
+            # For some reason the app calls _put_lock_request after setting auto lock
+            return self._put_lock_request(lock)
         return success
